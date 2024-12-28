2f50c520b2817aca0acf774c041071b6b41b999d
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2024-12-28 10:21:39 +0800

    chore: upgrade dependencies

--- src/resolve.ts
+++ src/resolve.ts
@@ -12,7 +12,24 @@ import {
 import { reusePendingPromises } from 'extra-promise'
 import { DiskCache, DiskCacheView, PassthroughKeyConverter } from 'extra-disk-cache'
 import { BraveJSON, IConverter } from 'brave-json'
-import { DNSClient, IPacket, IQuestion, OPCODE, QR, RCODE } from 'extra-dns'
+import {
+  DNSClient
+, IPacket
+, IQuestion
+, OPCODE
+, QR
+, RCODE
+, A_RDATA
+, AAAA_RDATA
+, CNAME_RDATA
+, MX_RDATA
+, NS_RDATA
+, PTR_RDATA
+, SOA_RDATA
+, AFSDB_RDATA
+, NAPTR_RDATA
+, SRV_RDATA
+} from 'extra-dns'
 import { timeoutSignal } from 'extra-abort'
 import { randomInt } from 'extra-rand'
 import { IServerInfo } from './utils/parse-server-info.js'
@@ -68,15 +85,143 @@ export async function createMemoizedResolve(
   } else {
     const converter: IConverter<
       ArrayBufferLike
+    | A_RDATA
+    | AAAA_RDATA
+    | CNAME_RDATA
+    | MX_RDATA
+    | NS_RDATA
+    | PTR_RDATA
+    | SOA_RDATA
+    | AFSDB_RDATA
+    | NAPTR_RDATA
+    | SRV_RDATA
     , [type: 'ArrayBuffer', number[]]
+    | [type: 'A_RDATA', { ADDRESS: string }]
+    | [type: 'AAAA_RDATA', { ADDRESS: string }]
+    | [type: 'CNAME_RDATA', { CNAME: string }]
+    | [type: 'MX_RDATA', {
+        PREFERENCE: number
+        EXCHANGE: string
+      }]
+    | [type: 'NS_RDATA', { NSDNAME: string }]
+    | [type: 'PTR_RDATA', { PTRDNAME: string }]
+    | [type: 'SOA_RDATA', {
+        MNAME: string
+        RNAME: string
+        SERIAL: number
+        REFRESH: number
+        RETRY: number
+        EXPIRE: number
+        MINIMUM: number
+      }]
+    | [type: 'AFSDB_RDATA', {
+        SUBTYPE: number
+        HOSTNAME: string
+      }]
+    | [type: 'NAPTR_RDATA', {
+        ORDER: number
+        PREFERENCE: number
+        FLAGS: string
+        SERVICES: string
+        REGEXP: string
+        REPLACEMENT: string
+      }]
+    | [type: 'SRV_RDATA', {
+        PRIORITY: number
+        WEIGHT: number
+        PORT: number
+        TARGET: string
+      }]
     > = {
       toJSON(value) {
         if (value instanceof ArrayBuffer) return ['ArrayBuffer', toArray(new Uint8Array(value))]
+        if (value instanceof A_RDATA) return ['A_RDATA', { ADDRESS: value.ADDRESS }]
+        if (value instanceof AAAA_RDATA) return ['AAAA_RDATA', { ADDRESS: value.ADDRESS }]
+        if (value instanceof CNAME_RDATA) return ['CNAME_RDATA', { CNAME: value.CNAME }]
+        if (value instanceof MX_RDATA) {
+          return ['MX_RDATA', {
+            PREFERENCE: value.PREFERENCE
+          , EXCHANGE: value.EXCHANGE
+          }]
+        }
+        if (value instanceof NS_RDATA) return ['NS_RDATA', { NSDNAME: value.NSDNAME }]
+        if (value instanceof PTR_RDATA) return ['PTR_RDATA', { PTRDNAME: value.PTRDNAME }]
+        if (value instanceof SOA_RDATA) {
+          return ['SOA_RDATA', {
+            MNAME: value.MNAME
+          , RNAME: value.RNAME
+          , SERIAL: value.SERIAL
+          , REFRESH: value.REFRESH
+          , RETRY: value.RETRY
+          , EXPIRE: value.EXPIRE
+          , MINIMUM: value.MINIMUM
+          }]
+        }
+        if (value instanceof AFSDB_RDATA) {
+          return ['AFSDB_RDATA', {
+            SUBTYPE: value.SUBTYPE
+          , HOSTNAME: value.HOSTNAME
+          }]
+        }
+        if (value instanceof NAPTR_RDATA) {
+          return ['NAPTR_RDATA', {
+            ORDER: value.ORDER
+          , PREFERENCE: value.PREFERENCE
+          , FLAGS: value.FLAGS
+          , SERVICES: value.SERVICES
+          , REGEXP: value.REGEXP
+          , REPLACEMENT: value.REPLACEMENT
+          }]
+        }
+        if (value instanceof SRV_RDATA) {
+          return ['SRV_RDATA', {
+            PRIORITY: value.PRIORITY
+          , WEIGHT: value.WEIGHT
+          , PORT: value.PORT
+          , TARGET: value.TARGET
+          }]
+        }
         throw new Error(`Unhandled raw ${value}`)
       }
     , fromJSON([type, value]) {
         switch (type) {
           case 'ArrayBuffer': return new Uint8Array(value).buffer
+          case 'A_RDATA': return new A_RDATA(value.ADDRESS)
+          case 'AAAA_RDATA': return new AAAA_RDATA(value.ADDRESS)
+          case 'CNAME_RDATA': return new CNAME_RDATA(value.CNAME)
+          case 'MX_RDATA': return new MX_RDATA(value.PREFERENCE, value.EXCHANGE)
+          case 'NS_RDATA': return new NS_RDATA(value.NSDNAME)
+          case 'PTR_RDATA': return new PTR_RDATA(value.PTRDNAME)
+          case 'SOA_RDATA': {
+            return new SOA_RDATA(
+              value.MNAME
+            , value.RNAME
+            , value.SERIAL
+            , value.REFRESH
+            , value.RETRY
+            , value.EXPIRE
+            , value.MINIMUM
+            )
+          }
+          case 'AFSDB_RDATA': return new AFSDB_RDATA(value.SUBTYPE, value.HOSTNAME)
+          case 'NAPTR_RDATA': {
+            return new NAPTR_RDATA(
+              value.ORDER
+            , value.PREFERENCE
+            , value.FLAGS
+            , value.SERVICES
+            , value.REGEXP
+            , value.REPLACEMENT
+            )
+          }
+          case 'SRV_RDATA': {
+            return new SRV_RDATA(
+              value.PRIORITY
+            , value.WEIGHT
+            , value.PORT
+            , value.TARGET
+            )
+          }
           default: throw new Error(`Unhandled type ${type}`)
         }
       }
